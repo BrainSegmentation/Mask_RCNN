eb07a15cc5e8eda6e972ab7f8baec81ea50aa1f1
atikinf <atikinf@gmail.com>
2018-12-20 11:39:10 +0100

    fully o p e r a t i o n a l

--- samples/braintissue/scripts/partition.py
+++ samples/braintissue/scripts/partition.py
@@ -2,6 +2,7 @@ import sys, os
 import glob
 from PIL import Image, ImageChops
 
+
 # Generates boxes which, together, partition the image into ~k**2 parts
 def get_partition_boxes(img, k):
     img_width, img_height = img.size
@@ -14,6 +15,7 @@ def get_partition_boxes(img, k):
             bounding_boxes.append(box)
     return bounding_boxes
 
+
 def get_partition_boxes_size(img, size):
     width, height = img.size
 
@@ -24,8 +26,9 @@ def get_partition_boxes_size(img, size):
             bounding_boxes.append(box)
     return bounding_boxes
 
+
 def get_overlapping_boxes_size(img, size, overlap):
-    if overlap > size:
+    if overlap >= size:
         exit
     width, height = img.size
 
@@ -33,8 +36,9 @@ def get_overlapping_boxes_size(img, size, overlap):
     for i in range(0, width, size - overlap):
         for j in range(0, height, size - overlap):
             box = (i, j, i + size, j + size)
-            bounding_boxex.append(box)
+            bounding_boxes.append(box)
     return bounding_boxes
+
 
 # Partitions an image
 def crop(image_path, k):
@@ -56,15 +60,27 @@ def mkdir(dir):
 
 
 # Takes partition (Image), path, section number, and file name
-def save_partition(img, out_path, name):
-    copy = Image.new('RGB', img.size, 255)
-    copy.paste(img)         
+def save_partition(img, out_path, name, format):
+    # copy = Image.new('RGB', img.size, 255)
+    # copy.paste(img)         
     path = os.path.join(out_path, name)
     mkdir(out_path)
-    copy.save(path)
+    img.save(path, format=format)
     
 
+def is_empty(img):
+    if img is None:
+        return True
+    colors = img.getcolors()
+    if len(colors) == 1:
+        return True
+    else:
+        return False
+
+
 def is_dense(img):
+    if img is None:
+        return False
     colors = img.getcolors()
     if len(colors) < 2 or colors[1][0] * 4 < colors[0][0]:
         return False
@@ -76,7 +92,6 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser(description='Image partitioner')
-
     # Optional
     parser.add_argument('--out', required=False,
                         default='./partitions/',
@@ -87,19 +102,24 @@ def main():
     parser.add_argument('--overlap', required=False, type=int,
                         default=0,
                         metavar='OVERLAP px')
+    parser.add_argument('--format', required=False, 
+                        default='png',
+                        metavar='OUTPUT FILE FORMAT')
     # Positional
     parser.add_argument('image', metavar='/path/to/image/')
     parser.add_argument('masks', metavar='/path/to/masks/')
     args = parser.parse_args()
 
+    # paths
     image_path = args.image
     mask_path = args.masks
-
     out_path = args.out
+    # config
     size = args.size
     overlap = args.overlap
+    format = args.format
     
-    # Hard Coded (but unused)
+    # Hard Coded (deprecated)
     k = 9                           # sqrt of # of partitions
 
     print('...................................................')
@@ -107,6 +127,7 @@ def main():
     print("Partition overlap: " + str(overlap) + '\n') 
 
     print('Outputting to ' + out_path)
+    print('Output format: ' + format)
 
     # Get locations of all mask files
     mask_paths = glob.glob(mask_path + '*.png')
@@ -116,42 +137,62 @@ def main():
 
     # Generate cropping bounding boxes
     img = Image.open(image_path)
-    bounding_boxes = get_partition_boxes_size(img, size)
+    bounding_boxes = get_overlapping_boxes_size(img, size, overlap)
 
+    name_end = ''
+    if 'tiff' in format.lower():
+        name_end = '.tif'
+    else:
+        name_end = '.png'
     # Crop all images according to each bounding box
     counter = 0
-    print('Partitioning Image(s) into <={} parts...'.format(len(bounding_boxes)))
+    print('Partitioning Image(s) into <= {} parts...'.format(len(bounding_boxes)))
     for i, box in enumerate(bounding_boxes):
-        sub_counter = 0;
+        sub_counter = 0
         # Make paths for current partition
         section_str = 'section-{}'.format(i)
         section_path = os.path.join(out_path, section_str)
         images_path = os.path.join(section_path, 'images')
-        masks_path = os.path.join(section_path, 'masks')
+        tissue_path = os.path.join(section_path, 'tissue_masks')
+        magnet_path = os.path.join(section_path, 'magnetic_masks')
         
         cumulative = None
-        save_buffer = []
+        tissue_buffer = []
+        magnet_buffer = []
+
         for j, mask in enumerate(mask_paths):
             mask_sub = cropToBox(mask, box)
-            name = section_str + '-mask-{}.png'.format(j)
-            save_buffer.append((mask_sub, name))
-            # ANDs buffers together into a cumulative image
-            if cumulative == None:
-                cumulative = Image.new('1', mask_sub.size)
-            cumulative = ImageChops.lighter(cumulative, mask_sub)
+            if not is_empty(mask_sub):
+                name = section_str + '-mask-{}'.format(j) + name_end
+                if 'tissue' in mask:
+                    tissue_buffer.append((mask_sub, name))
+                else:
+                    magnet_buffer.append((mask_sub, name))
+
+                # Overlays buffers into a cumulative image
+                if cumulative is None:
+                    cumulative = Image.new('1', mask_sub.size)
+                cumulative = ImageChops.lighter(cumulative, mask_sub)
         
         if is_dense(cumulative):
-            for image in save_buffer:
-                save_partition(image[0], masks_path, image[1])  # saves to masks/
+            for image in tissue_buffer:
+                save_partition(image[0], tissue_path, image[1], format)
                 counter += 1
                 sub_counter += 1
-            name = section_str + '.png'
+            for image in magnet_buffer:
+                save_partition(image[0], magnet_path, image[1], format) 
+                counter += 1
+                sub_counter += 1
+            name = section_str + name_end
             wafer_sub = cropToBox(image_path, box)
-            save_partition(wafer_sub, images_path, name) # saves to images/
+            save_partition(wafer_sub, images_path, name, format) # saves to images/
             counter += 1        
             sub_counter += 1
              
-        print('Wrote {0} images to {1}'.format(sub_counter, section_path))
+        if sub_counter == 0:
+            print('Saved 0 images from {}'.format(section_str))
+        else:
+            print('Saved {0} images to {1}'.format(sub_counter, section_path))
         sub_counter = 0
 
     print('Saved {0} images to {1}'.format(counter, out_path))
